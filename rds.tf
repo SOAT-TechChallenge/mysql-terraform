6c4b592c855a02f420c61f38346969496da2c7ad
Leyner Bueno <leyner_henrique@hotmail.com.br>
2025-10-06 10:50:20 -0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "mysql_database" {
   password = var.db_password
   port     = 3306
 
+
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   publicly_accessible    = false
